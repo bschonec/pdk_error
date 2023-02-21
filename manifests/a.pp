e82bc65f886e181efaf58dac8f6dbfe37c8b3f56
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-02-21 13:43:16 -0500

    first commit

--- manifests/a.pp
+++ manifests/a.pp
@@ -1,4 +1,8 @@
+if ($facts['os']['family'] == 'Redhat' and versioncmp($facts['os']['release']['major'], '8') <= -1) {
 
-  if ($facts['os']['family'] == 'Redhat' and versioncmp($facts['os']['release']['major'], '8') <= -1) {
-    fail("Puppetboard is not supported on ${facts['facts['os']['family']']}  ${facts['facts['os']['release']['major']']}.")
-  }
+  # This passes
+  fail("Puppetboard is not supported on ${facts.os.family} ${facts.os.release.major}.")
+
+  # This fails.
+  fail("Puppetboard is not supported on ${facts['facts['os']['family']']}  ${facts['facts['os']['release']['major']']}.")
+}
